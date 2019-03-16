6177e66551c58b33fcddfdd0e1532ce398673bb5
wsk <1261709167@qq.com>
2019-03-16 13:17:15 +0800

    V1.3.3 修复已知BUG。 加强某些卡牌，削弱某些卡牌。

--- src/main/java/com/wsk/cards/AbstractChooseCard.java
+++ src/main/java/com/wsk/cards/AbstractChooseCard.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 /**
  * @author wsk1103
  * @date 2019/3/5
- * @description 描述
+ * @description 定义打出卡牌时，可以选择打出的效果
  */
 public abstract class AbstractChooseCard extends CustomCard {
     public AbstractChooseCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
